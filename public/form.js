cb98dd2836a26a8dcd9e9920faa98a29abf4caf8
ollie <olivinanzali07@gmail.com>
2023-04-17 11:28:22 -0400

    adding files to repo

---
+++ public/form.js
@@ -0,0 +1,30 @@
+function createUser(formData){
+    const user = {
+    firstName: formData.get('firstName'),
+    lastName: formData.get('lastName'),
+    email: formData.get('email'),
+    password: formData.get('password'),
+  };
+  return user;
+};
+
+const form = document.getElementById('signup-form');
+
+form.addEventListener('submit', async(evt)=> {
+    evt.preventDefault();
+    const formData = new FormData(form);
+    const user = createUser(formData);
+
+    const res = await fetch('/', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(user),
+    });
+
+    const data = await res.json();
+    console.log(data);
+});
+
+export const newUser = createUser;
